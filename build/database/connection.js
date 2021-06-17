2691f207101fc961d0f8de9ffb0cc08727a47863
Pedro917 <pedrobarbosa.first@gmail.com>
2021-06-17 14:37:47 -0300

    deploy

--- build/database/connection.js
+++ build/database/connection.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var typeorm_1 = require("typeorm");
 typeorm_1.createConnection({
-    type: 'mysql',
-    url: 'mysql://root:!Admin123456@127.0.0.1:3306/dreams'
+    type: 'postgres',
+    url: 'postgres://rtcdvkwarqfmjr:9b5e565228ae52c7fb48f90797f91dbe11442c6dd7c41a8cb6b0009ade6b31fe@ec2-34-230-115-172.compute-1.amazonaws.com:5432/d103ivsl84covr'
 })
     .then(function () {
     console.log("📦 Conexão realizada com sucesso!");
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,8 @@
 import {createConnection} from "typeorm";
 
 createConnection({
-    type: 'mysql',
-    url: 'mysql://root:!Admin123456@127.0.0.1:3306/dreams'
+    type: 'postgres',
+    url: 'postgres://rtcdvkwarqfmjr:9b5e565228ae52c7fb48f90797f91dbe11442c6dd7c41a8cb6b0009ade6b31fe@ec2-34-230-115-172.compute-1.amazonaws.com:5432/d103ivsl84covr'
 })
     .then(() => {
         console.log("📦 Conexão realizada com sucesso!")
